3c71c6fa96dfb7a1774a19b9809dc529cf9859ea
Narendra <narendramunji21@gmail.com>
2024-05-08 21:49:25 +0530

    new project

---
+++ Aproject/springboot-restapi-consumer/src/main/java/com/example/demo/SpringbootRestapiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiConsumerApplication.class, args);
+	}
+
+}
